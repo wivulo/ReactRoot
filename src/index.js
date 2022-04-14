81a1b2443c0a4ff9d207522fb7c4f510447d867e
Walter Ivulo <81820374+wivulo@users.noreply.github.com>
2022-04-14 21:26:56 +0100

    primeu

---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from 'App'
+
+ReactDOM.render(<App />, document.getElementById("root"));
